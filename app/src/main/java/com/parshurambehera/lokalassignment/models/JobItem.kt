50ea3f3ef81fc023de06b3e6a4a8f8b9aad025b6
Parshuramb7 <parshurambehera7735@gmail.com>
2024-08-30 21:42:19 +0530

    Fragmet Added

---
+++ app/src/main/java/com/parshurambehera/lokalassignment/models/JobItem.kt
@@ -0,0 +1,9 @@
+package com.parshurambehera.lokalassignment.models
+
+data class JobItem(
+    val id: String,
+    val title: String,
+    val location: String,
+    val salary: String,
+    val phone: String
+)
